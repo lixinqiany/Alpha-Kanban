8a17bad0c9cd232a3a5b299ca4a02395913cf4ce
lixinqiany <lixinqiany@student.unimelb.edu.au>
2026-02-24 01:04:04 +0800

    [ Feature ] 抽取通用svgIcon

---
+++ frontend/src/components/SvgIcon/index.tsx
@@ -0,0 +1,41 @@
+import { defineComponent, h, type Component, type CSSProperties, type PropType } from 'vue'
+import styles from './SvgIcon.module.css'
+
+export default defineComponent({
+  name: 'SvgIcon',
+  props: {
+    icon: {
+      type: Object as PropType<Component>,
+      required: true,
+    },
+    size: {
+      type: Number,
+    },
+    width: {
+      type: Number,
+    },
+    height: {
+      type: Number,
+    },
+    color: {
+      type: String,
+    },
+  },
+  setup(props) {
+    return () => {
+      const w = props.width ?? props.size
+      const ht = props.height ?? props.size
+
+      const style: CSSProperties = {}
+      if (w != null) style.width = `${w}px`
+      if (ht != null) style.height = `${ht}px`
+      if (props.color) style.color = props.color
+
+      return (
+        <span class={styles.icon} style={style}>
+          {h(props.icon!)}
+        </span>
+      )
+    }
+  },
+})
